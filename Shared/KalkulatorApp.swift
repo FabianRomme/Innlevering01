bc40d61001d2fabfa5397c30e1e776298e8dc81e
FabianRomme <Fabian.romme@gmail.com>
2022-09-09 11:31:57 +0200

    Initial Commit

---
+++ Shared/KalkulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulatorApp.swift
+//  Shared
+//
+//  Created by Fabian Ulrik Romme on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct KalkulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

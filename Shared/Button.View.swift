79a68893f790bb7529c7cbc22dda9f2a34e9479e
FabianRomme <Fabian.romme@gmail.com>
2022-09-21 13:28:36 +0200

    initial

---
+++ Shared/Button.View.swift
@@ -0,0 +1,21 @@
+//
+//  Button.View.swift
+//  Kalkulator
+//
+//  Created by Fabian Ulrik Romme on 09/09/2022.
+//
+
+import SwiftUI
+
+struct Button_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Button_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Button_View()
+    }
+}
+
